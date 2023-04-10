ce3198f6fc3cd3b1b2716b2f242c81173cf197a0
Alif Naywa <naywaaalif@gmail.com>
2023-04-10 10:07:45 +0700

    Program PHP

---
+++ Modul_8/latihan1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Demo</title>
+    </head>
+
+    <body>
+
+    <?php
+    echo 'Kode PHP di sini';
+    // ...
+    ?>
+
+    <p>Dokumen HTML</p>
+
+    <?php
+    echo 'Kode PHP di sini';
+    // ...
+    ?>
+    
+    <!--Shortcut-->
+    <p> Kode <?php echo "PHP"; ?></p>
+    
+    </body>
+</html>
