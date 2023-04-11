49ae640b9cc90158187d52f54e5291220d5dc159
Alif Naywa <naywaaalif@gmail.com>
2023-04-11 18:37:33 +0700

    Deklarasi dan inisialisasi

---
+++ Modul_8/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo Variable</title>
+</head>
+
+<body>
+
+    <?php
+
+    // Deklarasi dan inisialisasi
+    $bil = 3;
+    echo $bil;
+
+    ?>
+
+</body>
+</html>
