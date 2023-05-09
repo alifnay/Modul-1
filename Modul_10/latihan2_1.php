a911cac4f72f7bf90df8b45128250ca30ee57118
Alif <naywaalif@gmail.com>
2023-05-09 14:02:03 +0700

    Menciptakan Session

---
+++ Modul_10/latihan2_1.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    if (isset($_POST['login'])) {
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+        //memeriksa login
+        if ($user == "Lebron" && $pass == "1234") {
+            //menciptakan session
+            $_SESSION['login'] = $user;
+            //menuju ke halaman pemeriksaan session
+            echo "<h1>Halo, kamu berhasil login! </h1>";
+            echo "<h2>Klik <a href='latihan2_2.php'> disini (latihan2_2.php)</a> untuk menuju ke halaman pemeriksaan session</h2>";
+        } 
+    } else {
+        ?>
+        <html>
+            <head>
+                <title>Login disini</title>
+            </head>
+            <body>
+                <form action="" method="post">
+                    <h2>Login disini</h2>
+                    Username    : <input type="text" name="user"><br>
+                    Password    : <input type="password" name="pass"><br>
+                    <input type="submit" name="login" value="login">
+                </form>
+            </body>
+        </html>
+    <?php
+    } ?>
+                
